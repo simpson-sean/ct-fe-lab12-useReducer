47baf8ca8695a5d2e42f61d9bce73ed17fa7cd93
Sean <simpson.sean@outlook.com>
2021-09-30 12:07:38 -0700

    WORKDONE: Refactored using useReducer; App functioning as expected

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import colorReducer, { initialValue } from '../../hooks/useColorChanger';
+
+
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [ state, dispatch ] = useReducer(colorReducer, initialValue);
+
+  return (
+    <>
+      <button aria-label="undo-button" onClick={ () => dispatch({type: 'undo'})}>undo</button>
+      <button aria-label="redo-button" onClick={ () => dispatch( {type: 'redo'} )}>redo</button>
+      <input aria-label="color-picker" type="color" value={state.current} onChange={({ target }) => dispatch( {type: 'selector', payload: target.value} )} />
+      <div aria-label="display" style={{ backgroundColor: state.current, width: '10rem', height: '10rem' }}></div>
+    </>
+  )
 }
+
+
